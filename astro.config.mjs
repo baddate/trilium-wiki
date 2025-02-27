505dcee03b890a72e328c05eeaa9c23d1ac48eb6
Mercas <me@smj.im>
2025-02-27 11:49:12 +0800

    fix: add site options

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://beta.trilium.smj.im',
     integrations: [
         starlight({
             title: 'Trilium Wiki',
